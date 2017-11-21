0ecfab0ea2b67154663d988c4dec78e04c7fdd3b
Huku <sevg@abv.bg>
2017-11-21 02:35:14 +0200

    add User.php

---
+++ User.php
@@ -0,0 +1,14 @@
+<?php
+
+class User{
+
+    public function profile(){
+        echo '<h1>Ti si v profila si</h1>';
+    }
+
+    public function register(){
+        echo '<h1>Ti si v reg formulqra</h1>';
+    }
+
+}
+
