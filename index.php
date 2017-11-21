7feee71884b86e3bec043f803daf32b0eaece72d
Huku <sevg@abv.bg>
2017-11-21 02:29:56 +0200

    uriParts

--- index.php
+++ index.php
@@ -1,22 +1,18 @@
 <?php
-echo 'Az sym index';
-
-//var_dump($_SERVER);
 $uri = $_SERVER['REQUEST_URI'];
+
 $junk = str_replace(
     basename(__FILE__),
     '',
     $_SERVER['PHP_SELF']
 );
-//var_dump($junk);
 
 $significantPart = str_replace(
     $junk,
     '',
     $uri
 );
-//var_dump($significantPart);
 
-$uriPart = explode('/', $significantPart);
-
+$uriParts = explode('/', $significantPart);
+var_dump($uriParts);
 
